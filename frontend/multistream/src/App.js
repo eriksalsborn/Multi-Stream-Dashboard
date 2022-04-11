caa1141d116ebd850cb55122b24dc2f061b2cd50
eriksalsborn <erik.salsborn@hotmail.com>
2022-04-11 15:53:15 +0200

    Frontend created. Register-page with forms. Current problem: CORS: ACAO

--- frontend/multistream/src/App.js
+++ frontend/multistream/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import Layouts from './Layouts';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+
+      <div className="header">
+        <NavLink exact activeClassName= "active" to="/"> Home</NavLink>
+        <NavLink activeClassName= "active" to="/register"> Register</NavLink>
+        <NavLink activeClassName= "active" to="/login"> Login</NavLink>
+        <NavLink activeClassName= "active" to="/layouts"> Layouts</NavLink>
+      </div>
+
+    <div className="content">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/layouts' element={<Layouts/>}/>
+      </Routes>
+    </div>
+
+    </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/multistream/src/Layouts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layouts = () => {
+    return (
+        <div>
+        This is the Layouts page!
+        </div>
+    )
+}
+
+export default Layouts;
